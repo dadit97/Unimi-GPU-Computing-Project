de7be5bd58eeff2ae3c4e4e8dec081074cf4639a
Davide Tricella <dadi.tricella@gmail.com>
2023-04-29 18:07:32 +0200

    Improvements

--- Source/GPU-Computing/kernel.cu
+++ Source/GPU-Computing/kernel.cu
@@ -2,8 +2,11 @@
 #include <iostream>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#define MAXWEIGHT 5
 
 using namespace std;
+
+
 
 int adj_matrix[10][10] = {
     {0, 1, 5, 4, 3, 2, 4, 2, 3, -1},
@@ -15,18 +18,80 @@ int adj_matrix[10][10] = {
     {4 ,-1 ,-1 ,5 ,-1 ,-1 ,0 ,-1 ,2 ,1},
     {2 ,2 ,-1 ,-1 ,-1 ,-1 ,-1 ,0 ,5 ,5},
     {3 ,-1 ,-1 ,-1 ,5 ,-1 ,2 ,5 ,0 ,-1},
-    {1 ,3 ,-1 ,1 ,4 ,-1 ,2 ,4 ,1 ,-1}
+    {1 ,3 ,-1 ,1 ,4 ,-1 ,2 ,4 ,1 ,0}
 };
-
-__global__ void helloFromGPU(void) {
-    int tID = threadIdx.x;
-    printf("Hello World from GPU (I'am thread = %d)!\n", tID);
-}
 
 __global__ void printThreadMatrixRow(int* matrix, int dimension) {
     int tID = threadIdx.x;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < dimension; i++) {
         printf("Thread %d, valore %d,%d: %d\n", tID, tID, i,  matrix[tID * dimension + i]);
+    }
+}
+
+__device__ bool areAllTrue(bool* vector, int dimension) {
+    for (int i = 0; i < dimension; i++) {
+        if (vector[i] == false) return false;
+    }
+    return true;
+}
+
+__device__ void initializeBoolVector(bool* vector, int dimension) {
+    for (int i = 0; i < dimension; i++) {
+        vector[i] = false;
+    }
+}
+
+__global__ void shortestPath(int* matrix, int dimension) {
+    // Each Thread computes the problem for the node with its tID index
+    int tID = threadIdx.x;
+    int sourceNodeIndex = tID;
+
+    // Boolean vector simulating the Vt set initialization
+    bool* Vt = (bool*)malloc((dimension) * sizeof(bool));
+    initializeBoolVector(Vt, dimension);
+    Vt[sourceNodeIndex] = true;
+
+    // l vector initialization
+    int* l = (int*)malloc(dimension * sizeof(int));
+    
+    // Getting direct connections with source node maintaining the column fixed
+    // and jumping from row to row
+    for (int i = 0; i < dimension; i++) {
+        if (Vt[i] == true) continue;
+        else {
+            l[i] = matrix[sourceNodeIndex + dimension * i];
+        }
+    }
+
+    // while V != Vt
+    while (!areAllTrue(Vt, dimension)) {
+        // Find the next vertex closest to source node
+        int closestWeigth = MAXWEIGHT;
+        int closestIndex = sourceNodeIndex;
+        for (int i = 0; i < dimension; i++) {
+            if (Vt[i] == true) continue;
+            if (l[i] != -1 && l[i] < closestWeigth) {
+                closestWeigth = l[i];
+                closestIndex = i;
+            }
+        }
+        //printf("Thread %d, closest index: %d, closest weight: %d \n", tID, closestIndex, closestWeigth);
+
+        // Add closest vertex to Vt
+        Vt[closestIndex] = true;
+
+        // Recompute l
+        for (int i = 0; i < dimension; i++) {
+            if (Vt[i] == true) continue;
+            int uvWeight = matrix[closestIndex * dimension + i];
+            if (l[i] > (l[closestIndex] + uvWeight)) {
+                l[i] = l[closestIndex] + uvWeight;
+            }
+        }
+    }
+    for (int i = 0; i < dimension; i++) {
+        if (tID == true) continue;
+        printf("Thread %d, l index: %d, weight: %d \n", tID, i, l[i]);
     }
 }
 
@@ -45,24 +110,24 @@ int main(void) {
     }
 
     int* gpu_matrix;
-    cudaError_t mallocError = cudaMalloc(&gpu_matrix, nodes * nodes * sizeof(int));
+    cudaError_t cudaError = cudaMalloc(&gpu_matrix, nodes * nodes * sizeof(int));
 
-    if (mallocError != cudaSuccess) {
-        printf("Errore durante l'allocazione della memoria sulla GPU: %s\n", cudaGetErrorString(mallocError));
+    if (cudaError != cudaSuccess) {
+        printf("Errore durante l'allocazione della memoria sulla GPU: %s\n", cudaGetErrorString(cudaError));
         exit(1);
     }
     printf("Allocazione della memoria sulla GPU completata\n");
 
-    mallocError = cudaMemcpy(gpu_matrix, matrix, nodes * nodes * sizeof(int), cudaMemcpyHostToDevice);
-    if (mallocError != cudaSuccess) {
-        printf("Errore durante la copia della matrice sulla GPU: %s\n", cudaGetErrorString(mallocError));
+    cudaError = cudaMemcpy(gpu_matrix, matrix, nodes * nodes * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaError != cudaSuccess) {
+        printf("Errore durante la copia della matrice sulla GPU: %s\n", cudaGetErrorString(cudaError));
         exit(1);
     }
     printf("Copia della matrice sulla GPU completata\n");
     free(matrix);
         
-    //helloFromGPU << <1, 10 >> > ();
-    printThreadMatrixRow<<<1, 10 >>> (gpu_matrix, nodes);
+    shortestPath<<<1, nodes >>>(gpu_matrix, nodes);
+
     cudaDeviceSynchronize();
     return 0;
 }
