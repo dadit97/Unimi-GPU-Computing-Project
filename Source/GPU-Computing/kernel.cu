99be24013aa27850d64c1b07144136e1803d99c0
Davide Tricella <dadi.tricella@gmail.com>
2023-05-02 22:27:35 +0200

    Algorithm completed

--- Source/GPU-Computing/kernel.cu
+++ Source/GPU-Computing/kernel.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #define MAXWEIGHT 5
@@ -9,16 +10,16 @@ using namespace std;
 
 
 int adj_matrix[10][10] = {
-    {0, 1, 5, 4, 3, 2, 4, 2, 3, -1},
-    {1, 0, 5, -1, 5, 3, -1, 2, -1, 5},
-    {5, 5, 0, 1, -1, 3, -1, -1, -1, 4},
-    {4, -1, 1, 0, -1, -1, 5, -1, -1, 3},
-    {3, 5,-1,-1 ,0 ,-1 ,-1 ,-1 ,5 ,2},
-    {2 ,3 ,3 ,-1 ,-1 ,0 ,-1 ,-1 ,-1 ,5},
-    {4 ,-1 ,-1 ,5 ,-1 ,-1 ,0 ,-1 ,2 ,1},
-    {2 ,2 ,-1 ,-1 ,-1 ,-1 ,-1 ,0 ,5 ,5},
-    {3 ,-1 ,-1 ,-1 ,5 ,-1 ,2 ,5 ,0 ,-1},
-    {-1 ,5 ,4 ,3 ,2 ,5 ,1 ,5 ,-1 ,0}
+    {0, 1, 5, 4, 3, 2, 4, 2, 3, 10},
+    {1, 0, 5, 10, 5, 3, 10, 2, 10, 5},
+    {5, 5, 0, 1, 10, 3, 10, 10, 10, 4},
+    {4, 10, 1, 0, 10, 10, 5, 10, 10, 3},
+    {3, 5,10,10 ,0 ,10 ,10 ,10 ,5 ,2},
+    {2 ,3 ,3 ,10 ,10 ,0 ,10 ,10 ,10 ,5},
+    {4 ,10 ,10 ,5 ,10 ,10 ,0 ,10 ,2 ,1},
+    {2 ,2 ,10 ,10 ,10 ,10 ,10 ,0 ,5 ,5},
+    {3 ,10 ,10 ,10 ,5 ,10 ,2 ,5 ,0 ,10},
+    {10 ,5 ,4 ,3 ,2 ,5 ,1 ,5 ,10 ,0}
 };
 
 __global__ void printThreadMatrixRow(int* matrix, int dimension) {
@@ -41,7 +42,7 @@ __device__ void initializeBoolVector(bool* vector, int dimension) {
     }
 }
 
-__global__ void shortestPath(int* matrix, int dimension) {
+__global__ void shortestPaths(int* matrix, int dimension, int* results) {
     // Each Thread computes the problem for the node with its tID index
     int tID = threadIdx.x;
     int sourceNodeIndex = tID;
@@ -58,26 +59,16 @@ __global__ void shortestPath(int* matrix, int dimension) {
     for (int i = 0; i < dimension; i++) {
         l[i] = matrix[sourceNodeIndex * dimension + i];
     }
-
-    /*if (tID == 1) {
-        printf("Initial l: ");
-        for (int i = 0; i < dimension; i++) {
-            printf("%d ", l[i]);
-        }
-        printf("\n");
-    }*/
-
     
     // while V != Vt
-    //while (!areAllTrue(Vt, dimension)) {
-    for (int x = 0; x < 10; x++) {
+    while (!areAllTrue(Vt, dimension)) {
 
-        int closestWeigth = 50;
+        int closestWeigth = 999999999;
         int closestIndex = sourceNodeIndex;
+
         // Find the next vertex closest to source node
         for (int i = 0; i < dimension; i++) {
             if (Vt[i] == true) continue;
-            if (l[i] == -1) continue;
             if (l[i] < closestWeigth) {
                 closestWeigth = l[i];
                 closestIndex = i;
@@ -88,7 +79,7 @@ __global__ void shortestPath(int* matrix, int dimension) {
         // Add closest vertex to Vt
         Vt[closestIndex] = true;
 
-        if (tID == 0) {
+        /*if (tID == 4) {
             printf("Thread %d, closestIndex:%d,  closestWeigth:%d\n", tID, closestIndex, closestWeigth);
             printf("l: ");
             for (int i = 0; i < dimension; i++) {
@@ -100,20 +91,20 @@ __global__ void shortestPath(int* matrix, int dimension) {
                 printf("%d ", Vt[i]);
             }
             printf("\n");
-        }
+        }*/
         
         // Recompute l
         for (int i = 0; i < dimension; i++) {
             if (Vt[i] == true) continue;
             int uvWeight = matrix[closestIndex * dimension + i];
-            if (uvWeight == -1) continue;
             l[i] = min(l[i], l[closestIndex] + uvWeight);
         }
     }
-    /*for (int i = 0; i < dimension; i++) {
-        if (tID == true) continue;
-        printf("Thread %d, l index: %d, weight: %d \n", tID, i, l[i]);
-    }*/
+
+    for (int i = 0; i < dimension; i++) {
+        results[tID * dimension + i] = l[i];
+    }
+
     free(Vt);
     free(l);
 }
@@ -132,36 +123,56 @@ int main(void) {
         }
     }
 
-    /* Debug array initialization
-    bool* debugArray = (bool*)malloc(nodes * sizeof(bool*));
-    bool* debugArrayDevice;
-    cudaMalloc(&debugArrayDevice, nodes * sizeof(bool));
-    index = 0;
-    for (int i = 0; i < nodes; i++) {
-        debugArray[index] = false;
-        index++;
-    }
-    cudaMemcpy(debugArrayDevice, debugArray, nodes * sizeof(bool), cudaMemcpyHostToDevice);*/
-
     int* gpu_matrix;
     cudaError_t cudaError = cudaMalloc(&gpu_matrix, nodes * nodes * sizeof(int));
 
     if (cudaError != cudaSuccess) {
-        printf("Errore durante l'allocazione della memoria sulla GPU: %s\n", cudaGetErrorString(cudaError));
+        printf("Errore during matrix allocation on GPU: %s\n", cudaGetErrorString(cudaError));
         exit(1);
     }
-    printf("Allocazione della memoria sulla GPU completata\n");
+    printf("Matrix allocation on GPU completed\n");
 
     cudaError = cudaMemcpy(gpu_matrix, matrix, nodes * nodes * sizeof(int), cudaMemcpyHostToDevice);
     if (cudaError != cudaSuccess) {
-        printf("Errore durante la copia della matrice sulla GPU: %s\n", cudaGetErrorString(cudaError));
+        printf("Error during matrix copy on GPU: %s\n", cudaGetErrorString(cudaError));
         exit(1);
     }
-    printf("Copia della matrice sulla GPU completata\n");
+    printf("Matrix copy on GPU completed\n");
     free(matrix);
-        
-    shortestPath<<<1, nodes >>>(gpu_matrix, nodes);
 
+    int* resultsMatrix;
+    int* results = (int*)malloc(nodes * sizeof(int));
+    cudaError = cudaMalloc(&resultsMatrix, nodes * nodes * sizeof(int));
+
+    if (cudaError != cudaSuccess) {
+        printf("Error during results matrix allocation on GPU: %s\n", cudaGetErrorString(cudaError));
+        exit(1);
+    }
+    printf("Results matrix allocation completed\n");
+    
+    auto begin = std::chrono::high_resolution_clock::now();
+    shortestPaths<<<1, nodes >>>(gpu_matrix, nodes, resultsMatrix);
     cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+
+    cudaError = cudaMemcpy(results, resultsMatrix, nodes * nodes * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaError != cudaSuccess) {
+        printf("Error during results copy on Host: %s\n", cudaGetErrorString(cudaError));
+        exit(1);
+    }
+    printf("Results copy on Host completed\n");
+
+    for (int i = 0; i < nodes; i++) {
+        for (int j = 0; j < nodes; j++) {
+            printf("%d ", results[i * nodes + j]);
+        }
+        printf("\n");
+    }
+
+    printf("Kernel execution time: %f\n", elapsed.count());
+
+    cudaFree(resultsMatrix);
+    free(results);
     return 0;
 }
