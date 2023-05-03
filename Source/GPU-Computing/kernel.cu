fa930cb8f08e5be343d217e856c352b1ab9397c5
Davide Tricella <dadi.tricella@gmail.com>
2023-05-03 22:29:20 +0200

    Sequential algorithm added

--- Source/GPU-Computing/kernel.cu
+++ Source/GPU-Computing/kernel.cu
@@ -3,7 +3,6 @@
 #include <chrono>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#define MAXWEIGHT 5
 
 using namespace std;
 
@@ -42,7 +41,66 @@ __device__ void initializeBoolVector(bool* vector, int dimension) {
     }
 }
 
-__global__ void shortestPaths(int* matrix, int dimension, int* results) {
+bool allTrue(bool* vector, int dimension) {
+    for (int i = 0; i < dimension; i++) {
+        if (vector[i] == false) return false;
+    }
+    return true;
+}
+
+void shortestPathsSequential(int* matrix, int dimension, int* results) {
+    for (int node = 0; node < dimension; node++) {
+        // Boolean vector simulating the Vt set initialization
+        bool* Vt = (bool*)malloc((dimension) * sizeof(bool));
+        for (int i = 0; i < dimension; i++) {
+            Vt[i] = false;
+        }
+        Vt[node] = true;
+
+        // l vector initialization
+        int* l = (int*)malloc(dimension * sizeof(int));
+
+        // Getting direct connections with source node
+        for (int i = 0; i < dimension; i++) {
+            l[i] = matrix[node * dimension + i];
+        }
+
+        // while V != Vt
+        while (!allTrue(Vt, dimension)) {
+
+            int closestWeigth = 999999999;
+            int closestIndex = node;
+
+            // Find the next vertex closest to source node
+            for (int i = 0; i < dimension; i++) {
+                if (Vt[i] == true) continue;
+                if (l[i] < closestWeigth) {
+                    closestWeigth = l[i];
+                    closestIndex = i;
+
+                }
+            }
+
+            // Add closest vertex to Vt
+            Vt[closestIndex] = true;
+
+            for (int i = 0; i < dimension; i++) {
+                if (Vt[i] == true) continue;
+                int uvWeight = matrix[closestIndex * dimension + i];
+                l[i] = min(l[i], l[closestIndex] + uvWeight);
+            }
+        }
+
+        for (int i = 0; i < dimension; i++) {
+            results[node * dimension + i] = l[i];
+        }
+
+        free(Vt);
+        free(l);
+    }
+}
+
+__global__ void shortestPathsParallel(int* matrix, int dimension, int* results) {
     // Each Thread computes the problem for the node with its tID index
     int tID = threadIdx.x;
     int sourceNodeIndex = tID;
@@ -78,20 +136,6 @@ __global__ void shortestPaths(int* matrix, int dimension, int* results) {
         
         // Add closest vertex to Vt
         Vt[closestIndex] = true;
-
-        /*if (tID == 4) {
-            printf("Thread %d, closestIndex:%d,  closestWeigth:%d\n", tID, closestIndex, closestWeigth);
-            printf("l: ");
-            for (int i = 0; i < dimension; i++) {
-                printf("%d ", l[i]);
-            }
-            printf("\n");
-            printf("Vt: ");
-            for (int i = 0; i < dimension; i++) {
-                printf("%d ", Vt[i]);
-            }
-            printf("\n");
-        }*/
         
         // Recompute l
         for (int i = 0; i < dimension; i++) {
@@ -110,6 +154,8 @@ __global__ void shortestPaths(int* matrix, int dimension, int* results) {
 }
 
 int main(void) {
+
+    //Kernel part
     cudaSetDevice(0);
 
     int nodes = 10;
@@ -138,10 +184,10 @@ int main(void) {
         exit(1);
     }
     printf("Matrix copy on GPU completed\n");
-    free(matrix);
+    
 
     int* resultsMatrix;
-    int* results = (int*)malloc(nodes * sizeof(int));
+    int* results = (int*)malloc(nodes * nodes * sizeof(int));
     cudaError = cudaMalloc(&resultsMatrix, nodes * nodes * sizeof(int));
 
     if (cudaError != cudaSuccess) {
@@ -150,11 +196,8 @@ int main(void) {
     }
     printf("Results matrix allocation completed\n");
     
-    auto begin = std::chrono::high_resolution_clock::now();
-    shortestPaths<<<1, nodes >>>(gpu_matrix, nodes, resultsMatrix);
+    shortestPathsParallel<<<1, nodes >>>(gpu_matrix, nodes, resultsMatrix);
     cudaDeviceSynchronize();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
 
     cudaError = cudaMemcpy(results, resultsMatrix, nodes * nodes * sizeof(int), cudaMemcpyDeviceToHost);
     if (cudaError != cudaSuccess) {
@@ -170,9 +213,26 @@ int main(void) {
         printf("\n");
     }
 
-    printf("Kernel execution time: %f\n", elapsed.count());
-
     cudaFree(resultsMatrix);
+
+    //Sequential part
+
+    // Results matrix re-initialization
+    for (int i = 0; i < nodes * nodes; i++) {
+        results[i] = 0;
+    }
+
+    shortestPathsSequential(matrix, nodes, results);
+
+    printf("Sequential algorithm completed\n");
+    for (int i = 0; i < nodes; i++) {
+        for (int j = 0; j < nodes; j++) {
+            printf("%d ", results[i * nodes + j]);
+        }
+        printf("\n");
+    }
+    
     free(results);
+    free(matrix);
     return 0;
 }
